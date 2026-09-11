15e258bd1c0cc004911611fa332f06d06e66696e
gangadharan2k7 <gangadharan2k7@gmail.com>
2026-09-11 11:19:15 +0530

    Add unit tests for emotion_detector function

--- test_emotion_detection.py
+++ test_emotion_detection.py
@@ -1,1 +1,11 @@
 
+import unittest
+from EmotionDetection.emotion_detection import emotion_detector
+
+class TestEmotionDetection(unittest.TestCase):
+    def test_emotion_detector(self):
+        res1 = emotion_detector("I am glad this happened")
+        self.assertEqual(res1['dominant_emotion'], 'joy')
+
+if __name__ == '__main__':
+    unittest.main()
